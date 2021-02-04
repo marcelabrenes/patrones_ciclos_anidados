982fef98ce9b27e3c3d54ecb5c41e0d4d8fbd9d8
Marcela Brenes Daniels <marce89455@gmail.com>
2021-02-03 19:39:03 -0600

    Instructions were added

---
+++ numeros.rb
@@ -0,0 +1,4 @@
+=begin
+Escribir un programa llamado numeros.rb , que reciba por linea de comandos la cantidad de lineas, y
+dibuje el siguiente patrón: 1 12 123 1234 12345
+=end
---
+++ patrones.rb
@@ -0,0 +1,4 @@
+=begin
+Escribir un programa llamado patrones.rb con métodos que reciban la cantidad de lineas y
+muestren por pantalla los siguientes patrones de *: O, I, Z, X, 0
+=end
